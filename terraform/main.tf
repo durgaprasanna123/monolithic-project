4816e47bda25bc070014dd5f79b0821565ee4d56
root <root@ip-172-31-29-115.eu-north-1.compute.internal>
2023-09-25 08:31:59 +0000

    file is ready

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,8 @@ secret_key = "9+8cVBivcve/GpxombNeo1/bl+ERb3grNeeOmDpx"
 
 
 resource "aws_instance" "key" { 
-ami = ""
+ami = "ami-086ee6e5ed2ea343i4"
+key_name = "mykey"
 instance_type = "t3.micro"
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
 tags = {
